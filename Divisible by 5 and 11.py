e27df31eb887c1ef6987f3ba713fdad4d046cb6c
ritusharma12345 <ritu.sharma_bca24@gla.ac.in>
2025-08-21 20:22:20 +0530

    Update Divisible by 5 and 11.py

--- Divisible by 5 and 11.py
+++ Divisible by 5 and 11.py
@@ -1,6 +1,5 @@
-A = int(input("Enter an integer: "))
-
-if A % 5 == 0 and A % 11 == 0:
-    print("Yes, A is divisible by both 5 and 11")
+n = int(input("Enter an integer: "))
+if n % 5 == 0 and n % 11 == 0:
+    print("Yes, n is divisible by both 5 and 11")
 else:
-    print("No, A is not divisible by both 5 and 11:")
+    print("No, n is not divisible by both 5 and 11:")
