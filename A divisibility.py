1a6dd5a1599ec5cee7ce3e9e81046ce8d298b2aa
ritusharma12345 <ritu.sharma_bca24@gla.ac.in>
2025-08-21 20:02:51 +0530

    Update A divisibility.py

--- A divisibility.py
+++ A divisibility.py
@@ -1,6 +1,6 @@
-A = int(input("Enter an integer: "))
-
-if A % 5 == 0 and A % 11 == 0:
+n = int(input("Enter an integer: "))
+if n % 5 == 0 and n % 11 == 0:
     print("Yes, divisible by both 5 and 11")
 else:
     print("No, not divisible by both 5 and 11")
+
